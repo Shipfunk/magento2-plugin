45f64b0b660f23fbd63218a329b33baf27863390
vuleticd <vuleticd@gmail.com>
2017-11-07 19:41:19 -0500

    selected pickup extension attributes, checkout relay to Mage REST API with shipping info, new DB tables old ones deleted

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -24,7 +24,8 @@ var config = {
             "Magento_Checkout/template/shipping-address/shipping-method-list": "Nord_Shipfunk/template/shipping-address/shipping-method-list",
             "Magento_Checkout/template/shipping-address/shipping-method-item": "Nord_Shipfunk/template/shipping-address/shipping-method-item",
             "Magento_Checkout/js/model/shipping-service": "Nord_Shipfunk/js/model/shipping-service-mixin",
-            "Magento_Checkout/template/shipping-information": "Nord_Shipfunk/template/shipping-information"
+            "Magento_Checkout/template/shipping-information": "Nord_Shipfunk/template/shipping-information",
+            "Magento_Checkout/js/model/shipping-save-processor/default": "Nord_Shipfunk/js/model/shipping-save-processor/default"
         }
     },
     deps: [
---
+++ view/frontend/web/js/model/shipping-save-processor/default.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'ko',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/model/resource-url-manager',
+    'mage/storage',
+    'Magento_Checkout/js/model/payment-service',
+    'Magento_Checkout/js/model/payment/method-converter',
+    'Magento_Checkout/js/model/error-processor',
+    'Magento_Checkout/js/model/full-screen-loader',
+    'Magento_Checkout/js/action/select-billing-address',
+    'Nord_Shipfunk/js/model/shipfunk-popup'
+], function (
+    ko,
+    quote,
+    resourceUrlManager,
+    storage,
+    paymentService,
+    methodConverter,
+    errorProcessor,
+    fullScreenLoader,
+    selectBillingAddressAction,
+    shipfunkPopup
+) {
+    'use strict';
+    
+    var selectedPickup = shipfunkPopup.getSelectedPickup();
+    return {
+        /**
+         * @return {jQuery.Deferred}
+         */
+        saveShippingInformation: function () {
+            var payload;
+
+            if (!quote.billingAddress()) {
+                selectBillingAddressAction(quote.shippingAddress());
+            }
+
+            payload = {
+                addressInformation: {
+                    'shipping_address': quote.shippingAddress(),
+                    'billing_address': quote.billingAddress(),
+                    'shipping_method_code': quote.shippingMethod()['method_code'],
+                    'shipping_carrier_code': quote.shippingMethod()['carrier_code'],
+                    'extension_attributes': {
+                        'pickup_name': selectedPickup().pickup_name,
+                        'pickup_addr': selectedPickup().pickup_addr,
+                        'pickup_postal': selectedPickup().pickup_postal,
+                        'pickup_city': selectedPickup().pickup_city,
+                        'pickup_country': selectedPickup().pickup_country,
+                        'pickup_id': selectedPickup().pickup_id,
+                        'pickup_openinghours': selectedPickup().pickup_openinghours,
+                        'pickup_openinghours_excep': selectedPickup().pickup_openinghours_excep
+                    }
+                }
+            };
+
+            fullScreenLoader.startLoader();
+
+            return storage.post(
+                resourceUrlManager.getUrlForSetShippingInformation(quote),
+                JSON.stringify(payload)
+            ).done(
+                function (response) {
+                    quote.setTotals(response.totals);
+                    paymentService.setPaymentMethods(methodConverter(response['payment_methods']));
+                    fullScreenLoader.stopLoader();
+                }
+            ).fail(
+                function (response) {
+                    errorProcessor.process(response);
+                    fullScreenLoader.stopLoader();
+                }
+            );
+        }
+    };
+});
